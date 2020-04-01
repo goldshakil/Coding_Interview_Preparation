37e4c962e43347fe7b48776de455ca426263f961
Zuhaib M.Shakeel (Dahab M.Shakeel) <48248272+goldshakil@users.noreply.github.com>
2020-04-01 21:22:37 +0900

    Update DynamicProgramming.java

--- Java Basics/DynamicProgramming.java
+++ Java Basics/DynamicProgramming.java
@@ -3,6 +3,10 @@ package tester;
 import java.util.*;
 import java.io.*;
 
+/*Typically, all the problems that require to maximize or minimize certain quantity
+or counting problems that say to count the arrangements under certain condition or certain probability 
+problems can be solved by using Dynamic Programming.
+*/
 class DynamicProgramming {
 	// Dynamic Programming is only a technique to make recursion faster
 	// No overlapping subproblems
