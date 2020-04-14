56f2e13fae6fde0781cb0bb70414640784a7c12a
Zuhaib M.Shakeel (Dahab M.Shakeel) <48248272+goldshakil@users.noreply.github.com>
2020-04-14 22:51:39 +0900

    Create Problem_21.java

---
+++ LeetCode/Problem_21.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode node=new ListNode(-1); // start building the merged linked list
+        ListNode head=node; // to never loose the head
+        
+        while(l1!=null&&l2!=null)
+        {
+            if(l1.val<l2.val)
+            {
+                node.next=l1;
+                l1=l1.next;
+            }
+            else
+           {
+                node.next=l2;
+                l2=l2.next;
+            }
+            node=node.next;
+        }
+        
+        if(l1!=null)
+        {
+            node.next=l1;
+            
+        }
+        if(l2!=null)
+        {
+              node.next=l2;
+        }
+        
+        return head.next;
+        
+        
+    
+    }
+}
