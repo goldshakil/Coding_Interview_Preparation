444f232f9619a4727e1359aa170ed75fe4dc0ce5
Zuhaib M.Shakeel (Dahab M.Shakeel) <48248272+goldshakil@users.noreply.github.com>
2020-04-29 23:53:48 +0900

    Update Problem_238.java

--- LeetCode/Problem_238.java
+++ LeetCode/Problem_238.java
@@ -31,3 +31,29 @@ class Solution {
 }
 
 */
+
+class Solution {
+	public int[] productExceptSelf(int[] nums) {
+		int product = 1;
+		int zero_flag = 0;
+		int left[] = new int[nums.length]; // store the prdouct to the left of index i
+		int right[] = new int[nums.length]; // store the product to the right of index i
+
+		left[0] = 1;
+		for (int i = 1; i < nums.length; i++) {
+			left[i] = left[i - 1] * nums[i - 1];
+		}
+
+		right[nums.length - 1] = 1;
+		for (int i = nums.length - 2; i >= 0; i--) {
+			right[i] = right[i + 1] * nums[i + 1];
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			left[i] = left[i] * right[i];
+		}
+
+		return left;
+
+	}
+}
