ae5c22c518b53f738ffdc30473f18a997a2ae062
Zuhaib M.Shakeel (Dahab M.Shakeel) <48248272+goldshakil@users.noreply.github.com>
2020-05-07 01:31:20 +0900

    Create Problem_141.java

---
+++ LeetCode/Problem_141.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        Set<ListNode> set= new HashSet<ListNode>();
+        
+        while(head!=null)
+        {
+            if(set.contains(head)) return true;
+            else
+            {
+                set.add(head);
+            }
+            
+            head=head.next;
+            
+        }
+        
+        return false;
+
+       
+    }
+}
