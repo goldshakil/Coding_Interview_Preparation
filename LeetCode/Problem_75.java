533f50948ac31be7dd1181eb32d5df0a6d4ed040
Zuhaib M.Shakeel (Dahab M.Shakeel) <48248272+goldshakil@users.noreply.github.com>
2020-06-03 22:01:19 +0900

    Update Problem_75.java

--- LeetCode/Problem_75.java
+++ LeetCode/Problem_75.java
@@ -1,43 +1,115 @@
 class Solution {
-	public void sortColors(int[] nums) {
+	public void sortColors(int[] a) {
+
+		// the key to this problem is swapping not direct replacing!!!
+        //why?: because we don't wanna loose any unscanned values
+        // move zeros to left and twos to the right
+        
+        int start=0; //next position for zero
+        int end=a.length-1; //next postition for one
+        int scanned=0;
+        
+        while(scanned<=end&&start<end)
+        {
+            if(a[scanned]==0)
+            {
+                a[scanned]=a[start]; //this is 100% -> 1
+                a[start]=0;
+                start++;
+                scanned++;
+            }
+            else if(a[scanned]==2)
+            {
+                a[scanned]=a[end]; //the value at the start can be anything so we have to recheck the same index (don't incremened scanned)
+                a[end]=2;
+                end--;
+            }
+            else
+            {
+                scanned++;
+            }
+        }
+	}
+}
+
+/*
+//counting sort
+class Solution {
+	public void sortColors(int[] a) {
 
 		// you can use any sorting function
 
-		int zeros = 0;
-		int ones = 0;
-		int twos = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] == 0) {
-				zeros++;
-			} else if (nums[i] == 1) {
-				ones++;
+		int lastZero = 0;
+		int nextOne = 0;
+		int lastTwo = a.length - 1;
+		int ending = 0;
+
+		for (int i = 0; i < a.length - ending; i++) {
+
+			if (a[i] == 0) {
+
+				a[i] = -1;// used
+
+				if (a[lastZero] != 1) {
+					a[lastZero] = 0;
+					lastZero++;
+					nextOne++;
+				}
+
+				else if (a[lastZero] == 1) {
+					a[nextOne] = 1; // don't lost the replaced one
+					nextOne++;
+					a[lastZero] = 0;
+					lastZero++;
+				}
+
 			}
 
-			else {
-				twos++;
+			else if (a[i] == 2) {
+
+				// a[i] = -1;// used
+
+				if (a[lastTwo] == 0) {
+					a[lastZero] = 0; // don't lost the zero
+					lastZero++;
+					nextOne++;
+
+					a[lastTwo] = 2;
+					lastTwo--;
+					ending++;
+
+				} else if (a[lastTwo] == 1) {
+					a[nextOne] = 1; // don't lose the replaced one
+					nextOne++;
+
+					a[lastTwo] = 2;
+					lastTwo--;
+					ending++;
+				}
+				// there is already a two there
+				else if (a[lastTwo] == 2) {
+					lastTwo--;
+				}
+				// empty
+				else {
+
+					a[lastTwo] = 2;
+					lastTwo--;
+					ending++;
+
+				}
 			}
-		}
-		int i = 0;
-		int counter = 0;
-		while (i < zeros) {
-			nums[counter] = 0;
 
-			counter++;
-			i++;
-		}
-		i = 0;
-		while (i < ones) {
-			nums[counter] = 1;
+			else if (a[i] == 1) {
 
-			counter++;
-			i++;
-		}
-		i = 0;
-		while (i < twos) {
-			nums[counter] = 2;
+				a[i] = -1;
+				a[nextOne] = 1;
+				nextOne++;
 
-			counter++;
-			i++;
+			}
+
 		}
 	}
 }
+
+*/
