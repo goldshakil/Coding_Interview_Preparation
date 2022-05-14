24e0c07bbe1606e7e4e437603da3b8d7eb3412b6
Dahab Shakeel (Zuhaib Shakeel) <48248272+goldshakil@users.noreply.github.com>
2022-05-14 21:23:33 +0900

    Create python_121.py

---
+++ LeetCode/Python/python_121.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:  
+        #O(N)
+        
+        if len(prices)==0:
+            return 0
+        
+        max_profit=0
+        window_profit=0
+        window_left=0
+        length_arr=len(prices)
+        
+        for i in range(1,length_arr,1):
+            if prices[i]-prices[window_left]<0: #Option 1: Start a new window since profit is negative -> found a minimum/better day to buy
+                window_profit=0
+                window_left=i
+            else:
+                window_profit=prices[i]-prices[window_left] #Option2: Add the new item to the windows since profit is positive -> viable window
+                
+            max_profit=max(max_profit,window_profit)
+                
+        return max_profit
+        
+
+        
+# O(N^2) -> Time exceeds
+#         max_profit=0
+        
+#         for idx,i in enumerate(prices):
+#             for j in prices[idx+1:]:
+#                 diff=j-i
+#                 if diff>0:
+#                     # a profit
+#                     max_profit=max(max_profit,diff)
+                    
+                    
+
+#         return max_profit
+        
