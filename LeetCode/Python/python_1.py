82e392ea28e03f622766be7eb34ecee3b50e206c
Dahab Shakeel (Zuhaib Shakeel) <48248272+goldshakil@users.noreply.github.com>
2022-05-14 17:17:52 +0900

    Create python_1.py

---
+++ LeetCode/Python/python_1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+#         O(N) Solution
+
+        # nums=[1,2,3,5,6,3]
+        # nums_hash={complement:[complement_indices]}
+        # nums_hash={1:[0] ,2:[1],3:[2,5],5:[3], 6:[4]}
+        nums_hash={}
+        for idx,i in enumerate(nums):
+            if nums_hash.get(i):
+                 nums_hash[i].append(idx)
+            else:
+                nums_hash[i]=[idx]
+                
+                
+        for idx, i in enumerate(nums):
+            complement=target-i
+            # complement found
+            if nums_hash.get(complement):
+                complement_indices=nums_hash.get(complement)
+                if i==complement and len(complement_indices)<=1:
+                    #case1: same element, same index
+                    pass
+                elif i==complement and len(complement_indices)>1:
+                    #case2: same element, different index
+                    return [complement_indices[0],complement_indices[1]]
+                else:
+                    #case3: different elements
+                    return [idx,complement_indices[0]]
+                    
+        
+#         O(N^2) solution 
+#         for i,item1 in enumerate(nums):
+#             for j,item2 in enumerate(nums):
+#                 if i==j:
+#                     continue
+#                 if item1+item2==target:
+#                     return [i,j]
+        
