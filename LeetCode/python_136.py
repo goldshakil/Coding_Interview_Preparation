1e9e5cf22ba68b9e0feac4b8e41059222a6192f7
Dahab Shakeel (Zuhaib Shakeel) <48248272+goldshakil@users.noreply.github.com>
2022-05-16 20:10:02 +0900

    Create python_136.py

---
+++ LeetCode/python_136.py
@@ -0,0 +1,16 @@
+#LC-136
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # O(N^2) -> double loop search
+        # O(N*LOG(N)) -> sort then linear search
+        # O(N) -> Create a hashmap then linear search -> O(N) Space
+        
+        # Keyword is "TWICE" -> XOR
+        
+        unique=0
+        
+        for i in nums:
+            unique^=i #Edge case of when len(nums)==1 is handled -> 0^ANY=ANY
+        
+        return unique
+            
