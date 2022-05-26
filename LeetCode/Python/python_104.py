5cc1a1ceab56a4ba6d305905547e23c030d99368
Dahab Shakeel (Zuhaib Shakeel) <omarshakil100@gmail.com>
2022-05-26 19:45:19 +0900

    Update python_104.py

--- LeetCode/Python/python_104.py
+++ LeetCode/Python/python_104.py
@@ -8,15 +8,15 @@
 
 
 # Think about 2 or 3 nodes
-def dfs(node,depth):
+def dfs(node):
     if not node: # null node -> tree height=0
         return 0 
-    return 1+max(dfs(node.left,depth),dfs(node.right,depth))  #+1 because we found a node
+    return 1+max(dfs(node.left),dfs(node.right))  #+1 because we found a node (add the depth coming up)
     
         
     
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # O(N) solution and O(N) space (linear binary tree case)
-        return dfs(root,0)
+        return dfs(root)
         
