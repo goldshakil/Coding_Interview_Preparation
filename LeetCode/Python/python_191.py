f06669b718629b05d24d2265ced3fb73b410b427
Dahab Shakeel (Zuhaib Shakeel) <omarshakil100@gmail.com>
2022-06-13 22:16:40 +0900

    Update python_191.py

--- LeetCode/Python/python_191.py
+++ LeetCode/Python/python_191.py
@@ -1,15 +1,31 @@
 # LC-191
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # O(N^2) -> double loop search
-        # O(N*LOG(N)) -> sort then linear search
-        # O(N) -> Create a hashmap then linear search -> O(N) Space
+    def hammingWeight(self, n: int) -> int:
         
-        # Keyword is "TWICE" -> XOR
+        # Since the loop will be run at max 32 time -> time is O(1)
         
-        unique=0 # 0 ^ X = X
+        mask=1
+        counter=0
         
-        for i in nums:
-            unique^=i #Edge case of when len(nums)==1 is handled -> 0^ANY=ANY
         
-        return unique
+        # Method 1: Shift n instead of mask
+        while n!=0:
+            bit = n&mask
+            if bit:
+                counter+=1          
+            n=n>>1
+            
+        # Method 2: Shifting the mask
+#         while n!=0:
+#             bit = n&mask
+#             if bit:
+#                 counter+=1
+#             # clear used bit in n
+#             inverted_mask=~mask
+#             n=n&inverted_mask
+            
+#             mask=mask<<1
+            
+        return counter
+            
+        
